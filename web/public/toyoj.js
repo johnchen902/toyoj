4409f408b3ecd8cb7b76f37dd8b0b2d15697e07f
John Chen <johnchen902@gmail.com>
2017-06-12 13:07:41 +0800

    Add confirmation dialogs to DELETE buttons.

--- web/public/toyoj.js
+++ web/public/toyoj.js
@@ -23,11 +23,42 @@
             console.error("jQuery unavailable after script-jquery loaded");
         });
     }
+    function openDialog(e) {
+        if(e[0].showModal) {
+            e[0].showModal();
+            return;
+        }
+        // sham
+        e.attr("open", true);
+        e.css("margin-left", "auto");
+        e.css("margin-right", "auto");
+        e.css("position", "fixed");
+        e.css("top", "50%");
+        e.css("transform", "translate(0, -50%)");
+    }
+    function closeDialog(e) {
+        if(e[0].close) {
+            e[0].close();
+            return;
+        }
+        // shim
+        e.attr("open", false);
+    }
     function jQueryReady() {
         $(".dismissible-message").append(
             $("<span>", {"class" : "close-button"})
             .text("\u274c")
             .click(e => $(e.currentTarget).parent().remove())
         );
+        $(".dangerous-button").each(function() {
+            var dialog = $("#" + $(this).data("dialog-id"));
+            var answerField = dialog.find(".answer-field");
+            var confirmButton = dialog.find(".confirm-button");
+            var cancelButton = dialog.find(".cancel-button");
+            $(this).click(() => openDialog(dialog));
+            cancelButton.click(() => closeDialog(dialog));
+            answerField.bind("propertychange change click keyup input paste",
+                    () => confirmButton.prop("disabled", answerField.val() != answerField.data("answer")));
+        });
     }
 })();
