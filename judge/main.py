15cef96675aa3f3cb160623d8b75359098c66523
John Chen <johnchen902@gmail.com>
2017-06-18 10:38:56 +0800

    Judge can now run tasks concurrently.

--- judge/main.py
+++ judge/main.py
@@ -11,20 +11,32 @@ class TaskFetchLoop:
         self._task_runner = task_runner
 
     async def run(self):
-        while True:
-            task = await self._fetch_task()
-            if task is None:
-                await asyncio.sleep(1)
-                continue
-            problem_id = task["problem_id"]
-            submission_id = task["submission_id"]
-            testcase_id = task["testcase_id"]
-            accepted, time, memory, verdict = \
-                    await self._task_runner.run(submission_id, testcase_id)
-            await self._write_result(problem_id, submission_id, testcase_id,
-                    accepted, time, memory, verdict)
+        task_set = set()
+        try:
+            while True:
+                task = await self._fetch_task()
+                task_set.add(asyncio.ensure_future(self._run_task(task)))
+                _, task_set = await asyncio.wait(task_set, timeout = 0)
+        finally:
+            if(task_set):
+                await asyncio.shield(asyncio.wait(task_set))
+
+    async def _run_task(self, task):
+        problem_id = task["problem_id"]
+        submission_id = task["submission_id"]
+        testcase_id = task["testcase_id"]
+        accepted, time, memory, verdict = \
+                await self._task_runner.run(submission_id, testcase_id)
+        await self._write_result(problem_id, submission_id, testcase_id,
+                accepted, time, memory, verdict)
 
     async def _fetch_task(self):
+        while True:
+            task = await self._fetch_task_nullable()
+            if task is not None:
+                return task
+            await asyncio.sleep(1)
+    async def _fetch_task_nullable(self):
         return await self._pool.fetchrow("""
             INSERT INTO result_judges (problem_id, submission_id, testcase_id, judge_name)
             (SELECT problem_id, submission_id, testcase_id, $1 AS judge_name
