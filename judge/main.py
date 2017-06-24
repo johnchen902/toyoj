1c8b63bafd6fc3a538d32bd59d10bc5312e6e393
John Chen <johnchen902@gmail.com>
2017-06-24 21:07:02 +0800

    Rewrite a bit.

--- judge/main.py
+++ judge/main.py
@@ -4,40 +4,29 @@ import asyncpg
 import signal
 import sys
 
-class TaskFetchLoop:
-    def __init__(self, judge_name, pool, task_runner):
+class JudgeTask:
+    def __init__(self, problem_id, submission_id, testcase_id):
+        self.problem_id = problem_id
+        self.submission_id = submission_id
+        self.testcase_id = testcase_id
+        self.accepted = None
+        self.time = None
+        self.memory = None
+        self.verdict = None
+
+class TaskFetcher:
+    def __init__(self, judge_name, pool):
         self._judge_name = judge_name
         self._pool = pool
-        self._task_runner = task_runner
 
-    async def run(self):
-        task_set = set()
-        try:
-            while True:
-                task = await self._fetch_task()
-                task_set.add(asyncio.ensure_future(self._run_task(task)))
-                _, task_set = await asyncio.wait(task_set, timeout = 0)
-        finally:
-            if(task_set):
-                await asyncio.shield(asyncio.wait(task_set))
-
-    async def _run_task(self, task):
-        problem_id = task["problem_id"]
-        submission_id = task["submission_id"]
-        testcase_id = task["testcase_id"]
-        accepted, time, memory, verdict = \
-                await self._task_runner.run(submission_id, testcase_id)
-        await self._write_result(problem_id, submission_id, testcase_id,
-                accepted, time, memory, verdict)
-
-    async def _fetch_task(self):
+    async def fetch(self):
         while True:
-            task = await self._fetch_task_nullable()
+            task = await self.fetch_nullable()
             if task is not None:
                 return task
             await asyncio.sleep(1)
-    async def _fetch_task_nullable(self):
-        return await self._pool.fetchrow("""
+    async def fetch_nullable(self):
+        row = await self._pool.fetchrow("""
             INSERT INTO result_judges (problem_id, submission_id, testcase_id, judge_name)
             (SELECT problem_id, submission_id, testcase_id, $1 AS judge_name
                 FROM results_view
@@ -45,25 +34,24 @@ class TaskFetchLoop:
                 LIMIT 1)
             RETURNING problem_id, submission_id, testcase_id
         """, self._judge_name)
-    async def _write_result(self, problem_id, submission_id, testcase_id,
-            accepted, time, memory, verdict):
-        return await self._pool.execute("""
-            INSERT INTO results (problem_id, submission_id, testcase_id,
-                accepted, time, memory, verdict)
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-        """, problem_id, submission_id, testcase_id,
-                accepted, time, memory, verdict)
+        if row is None:
+            return None
+        return JudgeTask(row["problem_id"], row["submission_id"],
+                         row["testcase_id"])
 
 class TaskRunner:
     def __init__(self, pool):
         self._pool = pool
 
-    async def run(self, submission_id, testcase_id):
-        submission = await self._fetch_submission(submission_id)
-        testcase = await self._fetch_testcase(testcase_id)
+    async def run(self, task):
+        submission = await self._fetch_submission(task.submission_id)
+        testcase = await self._fetch_testcase(task.testcase_id)
         print("Running ", submission, testcase, "...")
         await asyncio.sleep(1.0)
-        return False, 0, 0, "XX"
+        task.accepted = False
+        task.time = 0
+        task.memory = 0
+        task.verdict = "XX"
 
     async def _fetch_submission(self, submission_id):
         return await self._pool.fetchrow("""
@@ -78,12 +66,38 @@ class TaskRunner:
             WHERE id = $1
         """, testcase_id)
 
+class TaskWriter:
+    def __init__(self, pool):
+        self._pool = pool
+
+    async def write(self, task):
+        return await self._pool.execute("""
+            INSERT INTO results (problem_id, submission_id, testcase_id,
+                accepted, time, memory, verdict)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+        """, task.problem_id, task.submission_id, task.testcase_id,
+                task.accepted, task.time, task.memory, task.verdict)
+
 async def run(dsn):
     async with asyncpg.create_pool(dsn, min_size = 1, max_size = 1) as pool:
+        task_fetcher = TaskFetcher("test-2", pool)
         task_runner = TaskRunner(pool)
-        task_fetch_loop = TaskFetchLoop("test-2", pool, task_runner)
+        task_writer = TaskWriter(pool)
 
-        await task_fetch_loop.run()
+        async def run_and_write(task):
+            await task_runner.run(task)
+            await task_writer.write(task)
+
+        pending = set()
+        try:
+            while True:
+                task = await task_fetcher.fetch()
+                future = asyncio.ensure_future(run_and_write(task))
+                pending.add(future)
+                _, pending_tasks = await asyncio.wait(pending, timeout = 0)
+        finally:
+            if(pending):
+                await asyncio.shield(asyncio.wait(pending))
 
 if len(sys.argv) != 2:
     print("Usage: ./main.py DSN", file = sys.stderr)
