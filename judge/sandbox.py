2572474deea927298ef964fc8fd4937662b7b841
John Chen <johnchen902@gmail.com>
2017-07-23 16:58:20 +0800

    Implement write of Python wrapper

--- judge/sandbox.py
+++ judge/sandbox.py
@@ -1,5 +1,8 @@
 import asyncio
 
+class SandboxError(Exception):
+    pass
+
 class Sandbox:
     def __init__(self):
         self._process = None
@@ -40,9 +43,30 @@ class Sandbox:
             if line == b"ok\n":
                 return result
             if line == b"error\n":
-                raise asyncio.IncompleteReadError(result, None)
+                raise SandboxError()
             count = int(line)
             result += await self._process.stdout.readexactly(count)
 
-    async def write(self, *args, **kwargs):
-        raise NotImplementedError()
+    async def write(self, filename, data, bufsiz = 8192):
+        if b"\0" in filename:
+            raise ValueError("filename contains null charactor")
+        if b"\n" in filename:
+            raise ValueError("filename contains newline")
+
+        self._process.stdin.write(b"write %b\n" % filename)
+
+        line = await self._process.stdout.readuntil()
+        if line == b"error\n":
+            raise SandboxError()
+        assert line == b"ready\n"
+
+        for i in range(0, len(data), bufsiz):
+            part = data[i : i + bufsiz]
+            self._process.stdin.write(b"%d\n%b" % (len(part), part))
+            await self._process.stdin.drain()
+        self._process.stdin.write(b"0\n")
+
+        line = await self._process.stdout.readuntil()
+        if line == b"error\n":
+            raise SandboxError()
+        assert line == b"ok\n"
