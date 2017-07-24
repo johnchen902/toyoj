92db3e202e1489a94671d5264f4d08a9e12f56fd
John Chen <johnchen902@gmail.com>
2017-07-25 02:35:59 +0800

    Remove jquery and use dialog polyfill.

--- web/public/toyoj.js
+++ web/public/toyoj.js
@@ -1,65 +1,61 @@
 (function() {
     "use strict";
-    console.log("initial readyState is " + document.readyState);
+    function handleDialogs() {
+        var dialogs = document.getElementsByTagName("dialog");
+        if (!dialogs.length)
+            return;
+        if (!dialogs[0].showModal) {
+            var head = document.getElementsByTagName("head")[0];
+            var styles = document.createElement("link");
+            styles.rel = "stylesheet";
+            styles.href = "https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.4.7/dialog-polyfill.min.css";
+            styles.integrity = "sha256-my10UQP6Yx+XEf/gZTpm5/i9DJzvA0NJlHQ+xP9JFHw=";
+            styles.crossOrigin = "anonymous";
+            head.appendChild(styles);
+            var script = document.createElement("script");
+            script.src = "https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.4.7/dialog-polyfill.min.js";
+            script.integrity = "sha256-HSjn+BtEjYhbskZXdsYdbwH9mRZK1Rf4iHmltSDDguU=";
+            script.crossOrigin = "anonymous";
+            head.appendChild(script);
+            script.onload = function() {
+                Array.prototype.forEach.call(dialogs,
+                        dialog => dialogPolyfill.registerDialog(dialog));
+                handleDialogs();
+            };
+            return;
+        }
+        Array.prototype.forEach.call(
+            document.getElementsByClassName("dangerous-button"),
+            function (element) {
+                var dialog = document.getElementById(element.dataset.dialogId);
+                var txtAnswer = dialog.getElementsByClassName("answer-field")[0];
+                var btnConfirm = dialog.getElementsByClassName("confirm-button")[0];
+                var btnCancel = dialog.getElementsByClassName("cancel-button")[0];
+                element.onclick = () => dialog.showModal();
+                btnCancel.onclick = () => dialog.close();
+                ["propertychange", "change", "click", "keyup", "input", "paste"]
+                    .forEach(function(eventType) {
+                        txtAnswer.addEventListener(eventType,
+                            () => btnConfirm.disabled = txtAnswer.value != txtAnswer.dataset.answer);
+                    });
+                element.disabled = false;
+            });
+    }
+    function domContentLoaded() {
+        Array.prototype.forEach.call(
+            document.getElementsByClassName("dismissible-message"),
+            function (element) {
+                var btnClose = document.createElement("span");
+                btnClose.appendChild(document.createTextNode("\u274c"));
+                btnClose.className = "close-button";
+                btnClose.onclick = () => element.remove();
+                element.appendChild(btnClose);
+            });
+        handleDialogs();
+    }
     if(document.readyState == "loading") {
         document.addEventListener("DOMContentLoaded", domContentLoaded);
     } else {
         domContentLoaded();
-    }
-    function domContentLoaded() {
-        if(window.jQuery) {
-            console.log("jQuery available after DOMContentLoaded");
-            jQueryReady();
-            return;
-        }
-        console.log("jQuery unavailable after DOMContentLoaded");
-        var s = document.getElementById("script-jquery");
-        s.addEventListener("load", function(event) {
-            if(window.jQuery) {
-                console.log("jQuery available after script-jquery loaded");
-                jQueryReady();
-                return;
-            }
-            console.error("jQuery unavailable after script-jquery loaded");
-        });
-    }
-    function openDialog(e) {
-        if(e[0].showModal) {
-            e[0].showModal();
-            return;
-        }
-        // sham
-        e.attr("open", true);
-        e.css("margin-left", "auto");
-        e.css("margin-right", "auto");
-        e.css("position", "fixed");
-        e.css("top", "50%");
-        e.css("transform", "translate(0, -50%)");
-    }
-    function closeDialog(e) {
-        if(e[0].close) {
-            e[0].close();
-            return;
-        }
-        // shim
-        e.attr("open", false);
-    }
-    function jQueryReady() {
-        $(".dismissible-message").append(
-            $("<span>", {"class" : "close-button"})
-            .text("\u274c")
-            .click(e => $(e.currentTarget).parent().remove())
-        );
-        $(".dangerous-button").each(function() {
-            var dialog = $("#" + $(this).data("dialog-id"));
-            var answerField = dialog.find(".answer-field");
-            var confirmButton = dialog.find(".confirm-button");
-            var cancelButton = dialog.find(".cancel-button");
-            $(this).click(() => openDialog(dialog));
-            cancelButton.click(() => closeDialog(dialog));
-            answerField.bind("propertychange change click keyup input paste",
-                    () => confirmButton.prop("disabled", answerField.val() != answerField.data("answer")));
-            $(this).prop("disabled", false);
-        });
     }
 })();
